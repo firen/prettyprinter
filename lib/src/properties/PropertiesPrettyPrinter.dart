9e76aa4085883b6f8de0715804c465c8f473e29c
firen <firen2k5@gmail.com>
2013-03-09 01:46:35 +0100

    add rendering line number

--- lib/src/properties/PropertiesPrettyPrinter.dart
+++ lib/src/properties/PropertiesPrettyPrinter.dart
@@ -9,22 +9,38 @@ class PropertiesPrettyPrinter {
 
   print( Properties properties ) {
     Iterator propertiesIterator = properties.getPropertiesIterator();
+    int lineNumber = 0;
+    
+    TableElement codeTable = new TableElement();
+    codeTable.classes = ["code-table"];
+    
     while( propertiesIterator.moveNext() ) {
       Entity current = propertiesIterator.current;
 
+      TableRowElement lineElement = new TableRowElement();
+      lineElement.classes = ["line"];
+      
+      TableCellElement lineNumberElement = new TableCellElement();
+      lineNumberElement.classes = ["line_no"];
+      lineNumberElement.text = (lineNumber++).toString();
+      lineElement.append(lineNumberElement);
+
       switch( current.getType() ) {
         case EntityTypes.PROPERTY:
-          DivElement propertyDivElement = new DivElement();
+          TableCellElement propertyDivElement = new TableCellElement();
+          propertyDivElement.classes = ["line_content"];
           propertyDivElement.innerHtml = "${current.key}<span style='color:blue'>=</span>${current.value}";
-          _targetDivElement.append(propertyDivElement);
+          lineElement.append(propertyDivElement);
           break;
         case EntityTypes.COMMENT:
-          DivElement propertyDivElement = new DivElement();
+          TableCellElement propertyDivElement = new TableCellElement();
+          propertyDivElement.classes = ["line_content"];
           propertyDivElement.innerHtml = "<span style='color:green'>${current.comment}</span>";
-          _targetDivElement.append(propertyDivElement);
+          lineElement.append(propertyDivElement);
           break;
       }
-
+      codeTable.append(lineElement);
     }
+    _targetDivElement.append(codeTable);
   }
 }
